0ab4df709620cefa685531c6823276ea0b5fba2e
Ult1 <56165675+Up05@users.noreply.github.com>
2024-02-03 16:10:44 +0200

    Update fun.c

--- fun.c
+++ fun.c
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+// Could, probably, be replaced with os.exists()/os.is_dir(), which would make the program more software independent, but I can't be botherred now...
+
 // completely from: https://stackoverflow.com/questions/6218325/how-do-you-check-if-a-directory-exists-on-windows-in-c
 BOOL dir_exists(LPCTSTR szPath)
 {
